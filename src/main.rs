77113c36a824c8b60b7bc37d79cba122648e651d
Jake Howard <git@theorangeone.net>
2019-06-07 13:50:56 +0100

    Everything works except lifetimes of T

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ extern crate executors;
 use executors::Executor;
 
 use executors::crossbeam_workstealing_pool::ThreadPool;
-use std::sync::mpsc::channel;
 
 
 struct Worker {
     pool: ThreadPool
 }
 
-trait Workable: Send + Sync {
+trait Workable: Send {
     fn execute(&self);
 }
 
@@ -22,25 +21,23 @@ impl Worker {
         }
     }
 
-    pub fn schedule<T: Workable + ?Sized>(&self, job: &'static T) {
-        self.pool.execute(move || {
-            job.execute()
+    pub fn schedule<T: Workable>(&self, job: T) {
+        self.pool.execute(|| {
+            job.execute();
         });
+    }
+}
+
+struct Job1;
+
+impl Workable for Job1 {
+    fn execute(&self) {
+        println!("Executed");
     }
 }
 
 
 fn main() {
-    let n_workers = 4;
-    let pool = ThreadPool::new(n_workers);
-    println!("Hello, world!");
-    let n_jobs = 8;
-
-    let (tx, rx) = channel();
-    for _ in 0..n_jobs {
-        let tx = tx.clone();
-        pool.execute(move|| {
-            tx.send(1).expect("channel will be there waiting for the pool");
-        });
-    }
+    let worker = Worker::start();
+    worker.schedule(Job1 {});
 }
