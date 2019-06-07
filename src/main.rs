20764efaff5bdb099ba88a584248e464d4d3c346
Jake Howard <git@theorangeone.net>
2019-06-07 20:54:14 +0100

    Give in and just define it as static
    It doesn't seem to cause any issues or memory leaks, which is great! It
    is still dropped when expected

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,48 @@ extern crate executors;
 use executors::Executor;
 
 use executors::crossbeam_workstealing_pool::ThreadPool;
-
+use std::{thread, time};
 
 struct Worker {
-    pool: ThreadPool
+    pool: ThreadPool,
 }
 
-trait Workable: Send {
+trait Workable: Send + Clone + 'static {
     fn execute(&self);
 }
 
-
 impl Worker {
-    pub fn start() -> Self {
+    pub fn start(threads: usize) -> Self {
         return Worker {
-            pool: ThreadPool::new(4)
-        }
+            pool: ThreadPool::new(threads),
+        };
     }
 
-    pub fn schedule<T: Workable>(&self, job: T) {
-        self.pool.execute(|| {
+    pub fn schedule(&self, job: impl Workable) {
+        self.pool.execute(move || {
+            println!("Executing job");
             job.execute();
+            println!("Executed job");
         });
     }
 }
 
-struct Job1;
+#[derive(Clone)]
+struct Job1 {
+    data: [i64; 1000],
+}
 
 impl Workable for Job1 {
     fn execute(&self) {
-        println!("Executed");
+        println!("Executed job on {:?}", thread::current().name());
     }
 }
 
-
 fn main() {
-    let worker = Worker::start();
-    worker.schedule(Job1 {});
+    let worker = Worker::start(num_cpus::get());
+    worker.schedule(Job1 {
+        data: [2000000000; 1000],
+    }); // Big data!
+    thread::sleep(time::Duration::from_millis(1000));
+    println!("Done");
 }
